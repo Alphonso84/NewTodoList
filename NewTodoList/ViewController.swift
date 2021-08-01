bdd64c241084342d78600a3ba77529e7ce5dc5fc
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-08-01 11:27:02 -0700

    Initial Commit

---
+++ NewTodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTodoList
+//
+//  Created by Alphonso Sensley II on 8/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
